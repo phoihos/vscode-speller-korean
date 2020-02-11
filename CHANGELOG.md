04136f417bbbfaa43a7c5876491350ec5ba8c13c
Jay Park <phoihos@hotmail.com>
2020-02-11 16:00:59 +0900

    Add context menu and icon

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,9 +2,5 @@
 
 All notable changes to the "speller-korean" extension will be documented in this file.
 
-Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
-
-## [Unreleased]
-
 ## [1.0.0] - 2020-02-11
 - Initial release
--- src/extension.ts
+++ src/extension.ts
@@ -16,46 +16,46 @@ interface RestResponse {
 	message: RestMessage;
 }
 
-async function fixSpell(text: string, eol: string) {
+async function correctSpell(text: string, eol: string) {
 	// naver api function
-	let _fixSpell = async function (toFix: string, eol: string) {
+	let _correctSpell = async function (toCorrect: string, eol: string) {
 		const BASE_URL = 'https://m.search.naver.com/p/csearch/ocontent/util/SpellerProxy?color_blindness=0&q=';
 		let rest = new restm.RestClient('vscode-extension');
 
 		try {
-			let response = await rest.get<RestResponse>(BASE_URL + encodeURIComponent(toFix));
+			let response = await rest.get<RestResponse>(BASE_URL + encodeURIComponent(toCorrect));
 			if (response.statusCode != 200)
 				throw new Error(`HTTP Error: ${response.statusCode}`);
 			else if (response.result == null)
 				throw new Error('HTTP Result is null');
 
-			let fixed = response.result.message.result.notag_html;
-			return fixed.replace(/<br>/g, eol).replace(/&lt;/g, '<').replace(/&gt;/g, '>');
+			let corrected = response.result.message.result.notag_html;
+			return corrected.replace(/<br>/g, eol).replace(/&lt;/g, '<').replace(/&gt;/g, '>');
 		}
 		catch (e) {
 			console.error((<Error>e).message);
 			vscode.window.showErrorMessage((<Error>e).message);
-			return toFix;
+			return toCorrect;
 		}
 	};
 
 	const MAX_TEXT_COUNT = 500;
 	let chunk = ((text.length - 1) / MAX_TEXT_COUNT) + 1;
-	let fixedText: string = "";
+	let correctedText: string = "";
 
 	for (let i = 0; i < chunk; ++i) {
 		let from = i * MAX_TEXT_COUNT;
 		let length = Math.min(text.length - from, MAX_TEXT_COUNT);
 
-		fixedText += await _fixSpell(text.substr(i * MAX_TEXT_COUNT, length), eol);
+		correctedText += await _correctSpell(text.substr(i * MAX_TEXT_COUNT, length), eol);
 	}
 
-	return fixedText;
+	return correctedText;
 }
 
 // this method is called when your extension is activated
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('speller.fix', async () => {
+	let commandCallback = async () => {
 		let editor = vscode.window.activeTextEditor;
 		if (editor == undefined)
 			return;
@@ -72,18 +72,20 @@ export function activate(context: vscode.ExtensionContext) {
 
 		for (let i = 0; i < selections.length; ++i) {
 			let selection = selections[i];
-			let fixed = await fixSpell(document.getText(selection), eol);
+			let corrected = await correctSpell(document.getText(selection), eol);
 
 			let options = {
 				undoStopBefore: (i == 0),
 				undoStopAfter: (i == selections.length - 1)
 			};
 
-			editor.edit((eb) => eb.replace(selection, fixed), options);
+			editor.edit((eb) => eb.replace(selection, corrected), options);
 		}
-	});
+	};
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(vscode.commands.registerCommand('speller.correct', commandCallback));
+	context.subscriptions.push(vscode.commands.registerCommand('speller.correct.document', commandCallback));
+	context.subscriptions.push(vscode.commands.registerCommand('speller.correct.selection', commandCallback));
 }
 
 // this method is called when your extension is deactivated
