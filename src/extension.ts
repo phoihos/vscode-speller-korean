0e18854d16ec0d97814ef8156529272a55fb417c
Jay Park <phoihos@gmail.com>
2020-09-29 19:04:25 +0900

    Refactor some codes

--- src/extension.ts
+++ src/extension.ts
@@ -1,85 +1,27 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import * as restm from 'typed-rest-client/RestClient';
+import { correctText, MAX_CHUNK_SIZE } from './textCorrector'
+import { sleep } from './utils'
 
-interface RestResult {
-	html: string;
-	errata_count: number;
-	origin_html: string;
-	notag_html: string;
-}
-
-interface RestMessage {
-	result: RestResult;
-}
-
-interface RestResponse {
-	message: RestMessage;
-}
-
-interface ProgressContext {
+interface IProgressContext {
 	max: number;
 	pos: number;
 	step: number;
 	ratio: number;
 }
 
-const API_BASE_URL = 'https://m.search.naver.com/p/csearch/ocontent/util/SpellerProxy?color_blindness=0&q=';
-const MAX_TEXT_COUNT = 500;
-
-async function correctSpell(text: string, eol: string, progressContext: ProgressContext) {
-	const _correctSpell = async (textChunk: string) => {
-		const _unescape = (str: string) => {
-			return str.replace(/<br>/g, eol)
-				.replace(/&lt;/g, '<')
-				.replace(/&gt;/g, '>')
-				.replace(/&quot;/g, '"');
-		}
-
-		try {
-			let rest = new restm.RestClient('vscode-extension');
-			let response = await rest.get<RestResponse>(API_BASE_URL + encodeURIComponent(textChunk));
-			if (response.statusCode != 200)
-				throw new Error(`HTTP Error: ${response.statusCode}`);
-			else if (response.result == null)
-				throw new Error('HTTP Result is null');
-
-			return _unescape(response.result.message.result.notag_html);
-		}
-		catch (e) {
-			console.error((<Error>e).message);
-			vscode.window.showErrorMessage((<Error>e).message);
-
-			return textChunk;
-		}
-	};
-
-	let chunk = Math.floor((text.length - 1) / MAX_TEXT_COUNT) + 1;
-	let correctedText: string = "";
-
-	for (let i = 0; i < chunk; ++i) {
-		let from = i * MAX_TEXT_COUNT;
-		let length = Math.min(text.length - from, MAX_TEXT_COUNT);
-
-		progressContext.step++;
-		correctedText += await _correctSpell(text.substr(i * MAX_TEXT_COUNT, length));
-	}
-
-	return correctedText;
-}
-
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	let commandCallback = async (editor: vscode.TextEditor) => {
-		let document = editor.document;
-		let selections = editor.selections;
+	const commandCallback = async (editor: vscode.TextEditor) => {
+		const document = editor.document;
+		const selections = editor.selections;
 		const eol = (document.eol == vscode.EndOfLine.LF) ? '\n' : '\r\n';
 
 		if (selections.length == 1 && selections[0].isEmpty) {
-			let lastLine = document.lineAt(document.lineCount - 1);
-			let lastCharacter = lastLine.range.end.character;
+			const lastLine = document.lineAt(document.lineCount - 1);
+			const lastCharacter = lastLine.range.end.character;
 			selections[0] = new vscode.Selection(0, 0, lastLine.lineNumber, lastCharacter);
 		}
 
@@ -87,25 +29,22 @@ export function activate(context: vscode.ExtensionContext) {
 
 		let i = selections.length;
 		while (i--) {
-			let selection = selections[i];
-			chunk += Math.floor((document.getText(selection).length - 1) / MAX_TEXT_COUNT) + 1;
+			const selection = selections[i];
+			chunk += Math.floor((document.getText(selection).length - 1) / MAX_CHUNK_SIZE) + 1;
 		}
 
-		let progressContext: ProgressContext = {
+		const progressContext: IProgressContext = {
 			max: chunk,
 			pos: 0,
 			step: 0,
 			ratio: (100 / chunk)
 		};
 
-		let timerId = setTimeout(() => {
+		const timerId = setTimeout(() => {
 			vscode.window.withProgress({
-				location: vscode.ProgressLocation.Notification,
-				cancellable: false,
-				title: 'Speller for Korean: Correcting...'
+				title: 'Speller for Korean: Correcting...',
+				location: vscode.ProgressLocation.Notification
 			}, async (progress) => {
-				const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
-
 				do {
 					if (progressContext.step > 0) {
 						progress.report({ increment: (progressContext.step * progressContext.ratio) });
@@ -124,14 +63,15 @@ export function activate(context: vscode.ExtensionContext) {
 
 		i = selections.length;
 		while (i--) {
-			let selection = selections[i];
-			let selectedText = document.getText(selection);
-			let correctedText = await correctSpell(document.getText(selection), eol, progressContext);
+			const selection = selections[i];
+			const selectedText = document.getText(selection);
+			const correctedText = await correctText(selectedText, eol,
+				() => progressContext.step++, vscode.window.showErrorMessage);
 
 			if (correctedText == selectedText)
 				continue;
 
-			let options = {
+			const options = {
 				undoStopBefore: (i == selections.length - 1),
 				undoStopAfter: (i == 0)
 			};
---
+++ src/textCorrector.ts
@@ -0,0 +1,61 @@
+import * as restm from 'typed-rest-client/RestClient';
+
+const SPELLER_API_URL = 'https://m.search.naver.com/p/csearch/ocontent/util/SpellerProxy?color_blindness=0&q=';
+
+interface IRestResult {
+    html: string;
+    errata_count: number;
+    origin_html: string;
+    notag_html: string;
+}
+
+interface IRestMessage {
+    result: IRestResult;
+}
+
+interface IRestResponse {
+    message: IRestMessage;
+}
+
+function unescape(str: string, eol: string) {
+    return str.replace(/<br>/g, eol)
+        .replace(/&lt;/g, '<')
+        .replace(/&gt;/g, '>')
+        .replace(/&quot;/g, '"');
+}
+
+async function correctTextChunk(textChunk: string, eol: string, onError?: (message: string) => void) {
+    try {
+        let rest = new restm.RestClient('vscode-extension');
+        let response = await rest.get<IRestResponse>(SPELLER_API_URL + encodeURIComponent(textChunk));
+        if (response.statusCode != 200)
+            throw new Error(`HTTP Error: ${response.statusCode}`);
+        else if (response.result == null)
+            throw new Error('HTTP Result is null');
+
+        return unescape(response.result.message.result.notag_html, eol);
+    }
+    catch (e) {
+        console.error((<Error>e).message);
+        onError?.((<Error>e).message);
+
+        return textChunk;
+    }
+};
+
+export const MAX_CHUNK_SIZE = 500;
+
+export async function correctText(text: string, eol: string, onStep?: () => void, onError?: (message: string) => void) {
+    let correctedText: string = "";
+
+    const chunkCount = Math.floor((text.length - 1) / MAX_CHUNK_SIZE) + 1;
+    for (let i = 0; i < chunkCount; ++i) {
+        const from = i * MAX_CHUNK_SIZE;
+        const length = Math.min(text.length - from, MAX_CHUNK_SIZE);
+
+        onStep?.();
+        correctedText += await correctTextChunk(text.substr(i * MAX_CHUNK_SIZE, length), eol, onError);
+    }
+
+    return correctedText;
+}
